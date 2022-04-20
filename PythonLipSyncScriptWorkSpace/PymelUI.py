1c40917d6f1efad5013046bc2389271af4c201b3
SwiggityWAVE <62301348+SwiggityWAVE@users.noreply.github.com>
2022-04-20 15:13:32 +0200

    Update PymelUI.py

--- PythonLipSyncScriptWorkSpace/PymelUI.py
+++ PythonLipSyncScriptWorkSpace/PymelUI.py
@@ -1,13 +1,16 @@
-
+from PySide2 import QtCore, QtWidgets
 import pymel.core as pm
 
-blendShapes = pm.general.ls(exactType = "blendShape")
-blendShapesAsString = []
-for blendshape in blendShapes:
-    blendShapesAsString.append(str(blendshape))
+def ChangeFilePathForAudioFile():
+    pm.fileDialog2(fileFilter="*.wav")
+    print("audioFile")
+  
+def ChangeFilePathForModel():
+    print("model")
+    
+def ChangeFilePathForConfig():
+    print("config")
 
-
-from PySide2 import QtCore, QtWidgets
 
 
 #MAIN LOOP
@@ -15,10 +18,62 @@ wid = QtWidgets.QWidget()
 wid.resize(1024, 1024)
 wid.setWindowTitle("Window")
 
+#File system
+
+filePathKeys = ["audioFile", "model", "config"]
+filePaths = {"audioFile" : "something.wav", "model" : "ModelPath", "config" : "ConfigPath"}
+filePathQLineEdit = []
+changeFilePathQPushButton = []
+
+index = 0
+for key in filePathKeys:
+    filePathQLineEdit.append(QtWidgets.QLineEdit(filePaths[key], parent = wid))
+    filePathQLineEdit[index].move(256, 48 * index)
+    
+    changeFilePathQPushButton.append(QtWidgets.QPushButton('ChangeFilePath', parent = wid))
+    changeFilePathQPushButton[index].move(400, 48 * index)
+    index += 1
+    
+changeFilePathQPushButton[0].clicked.connect(ChangeFilePathForAudioFile)
+changeFilePathQPushButton[1].clicked.connect(ChangeFilePathForModel)
+changeFilePathQPushButton[2].clicked.connect(ChangeFilePathForConfig)
+
+#Viseme System
+#Lists all blendshapes in scene
+blendShapes = pm.general.ls(exactType = "blendShape")
+blendShapesAsString = []
+for blendshape in blendShapes:
+    blendShapesAsString.append(str(blendshape))
 
 
-DropDown = QtWidgets.QComboBox(wid)
-DropDown.addItems(blendShapesAsString)
+visemeList = ["Silent: ", "PP: ", "FF: ", "TH: ", "DD: ", "kk: ", "CH: ", "SS: ", "nn: ", "RR: ", "aa: ", "E: ", "I: ", "O: ", "U: "]
+visemeLabel = []
+visemeQComboBox = []
+
+
+index = 0
+for viseme in visemeList:
+    visemeLabel.append(QtWidgets.QLabel(wid))
+    visemeLabel[index].setText(visemeList[index])
+    visemeLabel[index].move(0, 48 * index)
+    
+    visemeQComboBox.append(QtWidgets.QComboBox(wid))
+    visemeQComboBox[index].addItems(blendShapesAsString)
+    visemeQComboBox[index].move(visemeLabel[index].width(), 48 * index)
+    
+    
+    index += 1
+
+
+
+
+
+############################################
+"""
+dropDown = QtWidgets.QComboBox(wid)
+dropDown.addItems(blendShapesAsString)
+dropDown.move(dropDownLabel.width(), 0)
+"""
 
 
 
