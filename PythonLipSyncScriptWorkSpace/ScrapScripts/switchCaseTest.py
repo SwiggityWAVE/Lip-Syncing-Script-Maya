8a03a995bfa8c092d3da383da5e4307a5ad73a8a
SwiggityWAVE <62301348+SwiggityWAVE@users.noreply.github.com>
2022-04-22 09:19:13 +0200

    Update switchCaseTest.py

--- PythonLipSyncScriptWorkSpace/ScrapScripts/switchCaseTest.py
+++ PythonLipSyncScriptWorkSpace/ScrapScripts/switchCaseTest.py
@@ -4,98 +4,98 @@
 case = 'j'
 undefinedChars = []
 
-if (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+if (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    PP                       Phonemes:      p, b, m                         Word example:           put, bat, mat
     print("yo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    FF                       Phonemes:      f, v                            Word example:           fat, vat
     print("yoo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    TH                       Phonemes:      th                              Word example:           think, that
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    DD                       Phonemes:      t, d                            Word example:           tip, doll
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    kk                       Phonemes:      k, g                            Word example:           call, gas
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    CH                       Phonemes:      tS, dZ, S                       Word example:           chair, join, she
     print("yooo")
 
 
 
 
-elif  (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    SS                       Phonemes:      s, z                            Word example:           sir, zeal
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    nn                       Phonemes:      n, l                            Word example:           lot, not
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
     #Viseme:    RR                       Phonemes:      r                               Word example:           red
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == 'æ') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    aa                       Phonemes:      A:                              Word example:           car
     print("yooo")
 
 
 
 
-elif  (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    E                       Phonemes:      e                                Word example:           bed
     print("yooo")
 
 
 
 
-elif  (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    I                       Phonemes:      ih                               Word example:           tip
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    O                       Phonemes:      oh                               Word example:           toe
     print("yooo")
 
 
 
 
-elif (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#') or (case == '#'):
+elif (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == '') or (case == ''):
     #Viseme:    U                       Phonemes:      ou                               Word example:           book
     print("yooo")
 
