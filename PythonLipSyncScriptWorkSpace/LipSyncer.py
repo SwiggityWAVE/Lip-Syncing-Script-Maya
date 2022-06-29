17353e2bee2eee975646a865387eb7ba70631a53
SwiggityWAVE <62301348+SwiggityWAVE@users.noreply.github.com>
2022-06-29 17:07:48 +0200

    Update LipSyncer.py

--- PythonLipSyncScriptWorkSpace/LipSyncer.py
+++ PythonLipSyncScriptWorkSpace/LipSyncer.py
@@ -1,6 +1,6 @@
 """
 This Lip-Syncing script was created by me Adrian Björkholm for my bachellor thesis.
-Please come and visit my site to see more of the stuff I have created, https://adrianbjorkholm.com/
+Please come and visit my site to see more of the stuff I have created https://adrianbjorkholm.com/ or my github at https://github.com/SwiggityWAVE
 
 Word class and the "transcribeFile" is inspiered by this code example:
     https://towardsdatascience.com/speech-recognition-with-timestamps-934ede4234b2
@@ -23,6 +23,9 @@ Instructions:
     The file format needs to be .wav and only have one channel in mono. The script does not support stereo sound.
     The audio file needs to be formatted to PCM. If the audiofile has the wrong configurations Maya might crash so make sure to save your scene before you run the script.
 
+    A voice transcription model needs to be attached to the script. You can download one here -> https://alphacephei.com/vosk/models
+    It's recommended only to generate lipsync animations in English because the transcription to phonetics conversion
+    the library is limited to only supporting English.
 
     The keyframes per second box need to have the same value as the animation timelines framerate.
     The "Silence Time-Criteria" and "Silence Cut-Down" boxes are configurable to the animator's preference.
@@ -30,6 +33,7 @@ Instructions:
     the mesh instructions to close the mouth between the words depending on how long the silence is between the words.
     "Silence Cut-Down" determines how much time it will take for the mesh to close its mouth since the silence was initiated.
 
+    If the blendshapes that are meant to be used were created after the script was loaded, you have to reload the script
 
 
 
